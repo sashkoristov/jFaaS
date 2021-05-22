f763702d09e16543ba82b8dd138f4562edbdaf1b
Stefan Kotrba <stefan.kotrba@uibk.students.ac.at>
2021-05-22 20:49:51 +0200

    fix alignment and @Override

--- src/main/java/jFaaS/invokers/GoogleFunctionInvoker.java
+++ src/main/java/jFaaS/invokers/GoogleFunctionInvoker.java
@@ -75,41 +75,36 @@ public class GoogleFunctionInvoker implements FaaSInvoker {
             HttpCredentialsAdapter adapter = new HttpCredentialsAdapter(tokenCredential);
             factory = transport.createRequestFactory(adapter);
             request = factory.buildPostRequest(genericUrl,content);
-
         }
-
-         else if(googleToken != null) {
-
+        else if(googleToken != null) {
             request = factory.buildPostRequest(genericUrl, content);
             String tokenValue = googleToken.substring(googleToken.indexOf('=')+1, googleToken.indexOf(','));
             request.getHeaders().setAuthorization("Bearer " + tokenValue);
-
         }
-         else {
+        else {
             request = factory.buildPostRequest(genericUrl, content);
+        }
 
-         }
-
-         assert request != null;
+        assert request != null;
         //Setting HTTP request Timeout to 60 Minutes for Cloud Functions that take more time
         request.setReadTimeout(3600000);
         response = request.execute();
-         assert response != null;
+        assert response != null;
 
-         try {
-             BufferedReader in = new BufferedReader(new InputStreamReader(response.getContent()));
-             String inputLine;
-             responseBuilder = new StringBuilder();
-             while ((inputLine = in.readLine()) != null) {
-                 responseBuilder.append(inputLine);
-             }
-             in.close();
-         }catch (Exception e){
-             e.printStackTrace();
-         }
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(response.getContent()));
+            String inputLine;
+            responseBuilder = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                responseBuilder.append(inputLine);
+            }
+            in.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
-         assert responseBuilder != null;
-         return new Gson().fromJson(responseBuilder.toString(), JsonObject.class).getAsJsonObject();
+        assert responseBuilder != null;
+        return new Gson().fromJson(responseBuilder.toString(), JsonObject.class).getAsJsonObject();
     }
 
 }
